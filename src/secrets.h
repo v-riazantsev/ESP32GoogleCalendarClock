7f41c070f117bcd8f2b78ef9f100d9f9f4038ff6
Vladimir Riazantsev <97821632+v-riazantsev@users.noreply.github.com>
2026-09-07 14:10:21 +0200

    Add example secrets.h

---
+++ src/secrets.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define WIFI_SSID "Wifi123"
+#define WIFI_PASSWORD "123"
+
+#define GOOGLE_APP_URL "https://script.google.com/macros/s/ASDsdsas...URL_TO_GOOGLE_APPS"
+#define GOOGLE_APP_TOKEN "TOKEN"
